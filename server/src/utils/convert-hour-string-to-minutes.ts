33fd88d1047585693e6a59699837b47c04997326
Raoni Cerqueira <raoni_henrique@live.com>
2022-10-11 19:18:40 -0300

    backend em node.js finalizado

--- server/src/utils/convert-hour-string-to-minutes.ts
+++ server/src/utils/convert-hour-string-to-minutes.ts
@@ -1,7 +1,7 @@
 // 18:00 -> 1080
 
 export function convertHourStringToMinutes(hourString: string) {
-    const [hours, minutes] = hourString.split(',').map(Number)
+    const [hours, minutes] = hourString.split(':').map(Number)
 
     const minutesAmount = (hours * 60) + minutes;
 
